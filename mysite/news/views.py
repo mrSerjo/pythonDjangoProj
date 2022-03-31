e99601476ecd7ed023b14cf957a00413f229179e
mrserjo <Glorytorus1488>
2022-03-31 16:49:55 +0300

    hw

--- mysite/news/views.py
+++ mysite/news/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import News
 
 
 def index(request):
-    return HttpResponse('Hello world')
+    news = News.objects.order_by('-created_at')
+    context = {'news': news, 'title': 'Список новостей'}
+    return render(request, 'news/index.html', context)
 
-
-def test(request):
-    return HttpResponse('Test page')
